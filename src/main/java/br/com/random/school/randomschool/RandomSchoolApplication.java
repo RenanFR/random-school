2fc1f5700c6f9a5185594fe0a64e017a5ec1c123
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-05-06 11:06:20 -0300

    first commit

---
+++ src/main/java/br/com/random/school/randomschool/RandomSchoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.random.school.randomschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/random/school/randomschool/RandomSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.random.school.randomschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
